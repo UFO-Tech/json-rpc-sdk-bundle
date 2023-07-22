a82d302c4891e3e1e7e9ac4625be571ee0099aa9
Ashterix <ashterix69@gmail.com>
2023-07-22 20:43:16 +0300

    add compiler pass

---
+++ src/DependencyInjection/SdkCompiler.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Ufo\JsonRpcSdkBundle\DependencyInjection;
+
+use Symfony\Bundle\MakerBundle\Str;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Ufo\RpcSdk\Interfaces\ISdkMethodClass;
+
+class SdkCompiler  implements CompilerPassInterface
+{
+
+    public function process(ContainerBuilder $container): void
+    {
+        // Витягуємо значення з конфігурації
+        $vendors = [];
+        $ns = $container->getParameter(Configuration::TREE_BUILDER_NAME . '.namespace');
+        foreach ($container->getParameter(Configuration::TREE_BUILDER_NAME . '.vendors') as $vendorData) {
+            $vendors[$ns . '\\' . Str::asCamelCase($vendorData['name'])] = $vendorData;
+        }
+
+        $services = $container->findDefinition(ISdkMethodClass::class);
+        foreach ($services as $id => $service) {
+            try {
+                $definition = $container->findDefinition($id);
+                $ref = new \ReflectionClass($definition->getClass());
+                $vendorData = $vendors[$ref->getNamespaceName()];
+                if (isset($vendorData['token'])
+                    && isset($vendorData['token_key'])
+                ) {
+                    $header = [
+                        $vendors[$ref->getNamespaceName()]['token_key'] => $vendors[$ref->getNamespaceName()]['token']
+                    ];
+                    $definition->setArgument('headers', $header);
+                }
+            } catch (\Throwable) {
+                continue;
+            }
+
+        }
+    }
+}
--- src/JsonRpcSdkBundle.php
+++ src/JsonRpcSdkBundle.php
@@ -4,7 +4,12 @@ namespace Ufo\JsonRpcSdkBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Ufo\JsonRpcSdkBundle\DependencyInjection\SdkCompiler;
 
 class JsonRpcSdkBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new SdkCompiler());
+    }
 }
