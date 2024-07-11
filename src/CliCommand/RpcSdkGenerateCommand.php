aac28ac285a4791635f897f59dbcd3a74262a506
maksim.trofimov <makx.work@gmail.com>
2024-07-11 12:23:55 +0300

    add optional vendor param for ufo:sdk:generate command

--- src/CliCommand/RpcSdkGenerateCommand.php
+++ src/CliCommand/RpcSdkGenerateCommand.php
@@ -5,16 +5,11 @@ namespace Ufo\JsonRpcSdkBundle\CliCommand;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Ufo\RpcSdk\Maker\Definitions\ClassDefinition;
-use Ufo\RpcSdk\Maker\Maker;
-use UfoCms\ColoredCli\CliColor;
-use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 #[AsCommand(
@@ -33,13 +28,22 @@ class RpcSdkGenerateCommand extends Command
         parent::__construct();
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->addOption('vendor', null, InputOption::VALUE_OPTIONAL, 'Optional vendor name');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $vendorName = $input->getOption('vendor');
+        $vendors = $this->getVendors($vendorName);
+
         $io = new SymfonyStyle($input, $output);
         try {
             $io->title("<options=bold>Start generate SDK from configs</>");
 
-            foreach ($this->getVendors() as $vendorData) {
+            foreach ($vendors as $vendorData) {
                 $command = $this->getApplication()->get('ufo:sdk:make');
                 $childInput = new ArrayInput([
                     'vendor' => $vendorData['name'],
@@ -61,12 +65,22 @@ class RpcSdkGenerateCommand extends Command
         }
     }
 
-    protected function getVendors(): array
+    protected function getVendors(?string $vendorName): array
     {
         $vendors = [];
         if ($this->container->hasParameter('json_rpc_sdk.vendors')) {
             $vendors = $this->container->getParameter('json_rpc_sdk.vendors');
         }
+
+        if ($vendorName !== null) {
+            foreach ($vendors as $vendor) {
+                if ($vendor['name'] === $vendorName) {
+                    return [$vendor];
+                }
+            }
+            return [];
+        }
+
         return $vendors;
     }
 }
